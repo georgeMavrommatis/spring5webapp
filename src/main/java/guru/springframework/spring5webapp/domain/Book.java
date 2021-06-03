7b7647ff4658fafcf02243431d48bb7ed91c26c5
George Mavrommatis <gmavrommatis@cognity.gr>
2021-06-03 18:06:51 +0300

    feat/section2
    BREAKING-CHANGE: adding Thymelead books list

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -40,7 +40,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
